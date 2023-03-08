2ffccc6bf2c641c25dc7a9f9ee1874b4abeb92a2
GitHub Action <action@github.com>
2023-03-08 22:56:08 +0000

    Template cleanup

--- src/main/kotlin/com/github/kryslynn23/jetbrainsintellijplatformplugintemplate/services/MyApplicationService.kt
+++ src/main/kotlin/com/github/kryslynn23/jetbrainsintellijplatformplugintemplate/services/MyApplicationService.kt
@@ -1,6 +1,6 @@
-package org.jetbrains.plugins.template.services
+package com.github.kryslynn23.jetbrainsintellijplatformplugintemplate.services
 
-import org.jetbrains.plugins.template.MyBundle
+import com.github.kryslynn23.jetbrainsintellijplatformplugintemplate.MyBundle
 
 class MyApplicationService {
 
